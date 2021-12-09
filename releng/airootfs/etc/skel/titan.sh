576f594e39bde788813dcd45e935868c8d065201
Jones <vrh81x@gmail.com>
2021-12-09 10:04:03 +0100

    fixed grub installation

--- releng/airootfs/etc/skel/titan.sh
+++ releng/airootfs/etc/skel/titan.sh
@@ -8,6 +8,8 @@ echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 echo root:4815 | chpasswd
 
 # GRUB install
+cd /boot/
+mkdir efi/
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 
